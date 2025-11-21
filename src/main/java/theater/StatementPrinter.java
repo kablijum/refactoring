dbdd1732bf27175eb9ed6ee093b56cf16f931c97
Jumana Kabli <kablijumana@gmail.com>
2025-11-21 07:49:52 -0500

    renaming performance to p becuse I did it with the helper method and forgot to commit

--- src/main/java/theater/StatementPrinter.java
+++ src/main/java/theater/StatementPrinter.java
@@ -51,24 +51,24 @@ public class StatementPrinter {
         return result.toString();
     }
 
-    private static int getAmount(Performance performance, Play play) {
+    private static int getAmount(Performance p, Play play) {
         int amount;
         switch (play.getType()) {
             case "tragedy":
                 amount = Constants.TRAGEDY_BASE_AMOUNT;
-                if (performance.getAudience() > Constants.TRAGEDY_AUDIENCE_THRESHOLD) {
+                if (p.getAudience() > Constants.TRAGEDY_AUDIENCE_THRESHOLD) {
                     amount += Constants.TRAGEDY_OVER_BASE_CAPACITY_PER_PERSON
-                            * (performance.getAudience() - Constants.TRAGEDY_AUDIENCE_THRESHOLD);
+                            * (p.getAudience() - Constants.TRAGEDY_AUDIENCE_THRESHOLD);
                 }
                 break;
             case "comedy":
                 amount = Constants.COMEDY_BASE_AMOUNT;
-                if (performance.getAudience() > Constants.COMEDY_AUDIENCE_THRESHOLD) {
+                if (p.getAudience() > Constants.COMEDY_AUDIENCE_THRESHOLD) {
                     amount += Constants.COMEDY_OVER_BASE_CAPACITY_AMOUNT
                             + (Constants.COMEDY_OVER_BASE_CAPACITY_PER_PERSON
-                            * (performance.getAudience() - Constants.COMEDY_AUDIENCE_THRESHOLD));
+                            * (p.getAudience() - Constants.COMEDY_AUDIENCE_THRESHOLD));
                 }
-                amount += Constants.COMEDY_AMOUNT_PER_AUDIENCE * performance.getAudience();
+                amount += Constants.COMEDY_AMOUNT_PER_AUDIENCE * p.getAudience();
                 break;
             default:
                 throw new RuntimeException(String.format("unknown type: %s", play.getType()));
